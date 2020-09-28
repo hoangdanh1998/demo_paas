d4a67db05ebace75d098d06b6eb99e68205ff371
Hoang-Danh <hoangdanhlx1998@gmail.com>
2020-09-28 14:03:05 +0700

    change

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello world"));
+app.get("/", (req, res) => res.send("Hello world 1"));
 app.listen(port, () => console.log("Application started"));
